f754856f15fdb042b523c7b633ad4d6323b0ec48
Farshid Mohammadi <fm708801@gmail.com>
2020-05-29 11:19:17 +0430

    Add project files.

---
+++ CRM_WebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRM_WebSite.MvcApplication" Language="C#" %>
---
+++ CRM_WebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRM_WebSite.Startup))]
+namespace CRM_WebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
